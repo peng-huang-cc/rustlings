00705f0ce007985db83ab947463f7aa2c4d1df4e
phcxc <h499871809@gmail.com>
2022-04-22 11:43:56 +0800

    standard_library_types solutions

--- exercises/standard_library_types/arc1.rs
+++ exercises/standard_library_types/arc1.rs
@@ -2,7 +2,7 @@
 // In this exercise, we are given a Vec of u32 called "numbers" with values ranging
 // from 0 to 99 -- [ 0, 1, 2, ..., 98, 99 ]
 // We would like to use this set of numbers within 8 different threads simultaneously.
-// Each thread is going to get the sum of every eighth value, with an offset.
+// Each thread is going to get the sum of every eighth svalue, with an offset.
 // The first thread (offset 0), will sum 0, 8, 16, ...
 // The second thread (offset 1), will sum 1, 9, 17, ...
 // The third thread (offset 2), will sum 2, 10, 18, ...
@@ -11,7 +11,6 @@
 
 // Because we are using threads, our values need to be thread-safe.  Therefore,
 // we are using Arc.  We need to make a change in each of the two TODOs.
-
 
 // Make this code compile by filling in a value for `shared_numbers` where the
 // first TODO comment is, and create an initial binding for `child_numbers`
@@ -26,11 +25,11 @@ use std::thread;
 
 fn main() {
     let numbers: Vec<_> = (0..100u32).collect();
-    let shared_numbers = // TODO
+    let shared_numbers = Arc::new(numbers);
     let mut joinhandles = Vec::new();
 
     for offset in 0..8 {
-        let child_numbers = // TODO
+        let child_numbers = shared_numbers.clone();
         joinhandles.push(thread::spawn(move || {
             let mut i = offset;
             let mut sum = 0;
